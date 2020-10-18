1b8e854071c0dc610e3906d4f6b2151aad789c0f
leeyc <leeyc924@naver.com>
2020-10-18 20:26:31 +0900

    페이지 라우터 수정

--- SNS-Service/routes/page.js
+++ SNS-Service/routes/page.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User } = require('../models');
 
 const router = express.Router();
 
@@ -20,11 +21,23 @@ router.get('/join', isNotLoggedIn, (req, res) => {
 });
 
 router.get('/', (req, res, next) => {
-    const twits = [];
-    res.render('main', {
-        title: 'NodeBird',
-        twits,
-    });
+    try {
+
+        const posts = await Post.findAll({
+            include: {
+                model: User,
+                attributes: ['id', 'nick'],
+            },
+            order: [['createAt', 'DESC']],
+        });
+        res.render('main', {
+            title: 'NodeBird',
+            twits: posts,
+        });
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
 });
 
 module.exports = router;
